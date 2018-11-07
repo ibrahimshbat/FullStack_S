4bcd9fca378b1e61d48209ea7abfa56c341ca55e
ibrahimshbat <ibrahimsabat@yahoo.com>
2018-11-07 16:16:39 +0000

    Add Codes for Full Stack Course

---
+++ codes/HellpWorld.py
@@ -0,0 +1,14 @@
+def is_friend(name):
+    if name[0]=='D':
+        return True
+    else:
+        return False
+
+print (is_friend('Dainal'))
+
+stooges = ['Moe','Larry','Curly']
+print (stooges)
+
+country = "Libya"
+
+print (country[::-1])
