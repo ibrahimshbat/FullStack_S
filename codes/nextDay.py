a19242987a4699666d7b26628166719d2c19461c
ibrahimshbat <ibrahimsabat@yahoo.com>
2018-11-21 22:28:05 +0000

    Fix bug

--- codes/nextDay.py
+++ codes/nextDay.py
@@ -8,12 +8,7 @@
 ###    nextDay(2012, 12, 30) => (2013, 1, 1)  (even though December really has 31 days)
 ###
 
-def nextDay(year, month, day):
-    """
-    Returns the year, month, day of the next day.
-    Simple version: assume every month has 30 days.
-    """
-    # YOUR CODE HERE
+def nextDay(year,month,day):
     if day < 30:
         return (year, month, day+1)
     else:
@@ -22,5 +17,4 @@ def nextDay(year, month, day):
                return (year+1, 1, 1)
             else:
                return (year, month+1,1)
-
- print nexDay(2012,12,30)
+print nextDay(2012,12,30)
