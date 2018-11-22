6a45cc263255815f19935187fdabc3660a07bde3
ibrahimshbat <ibrahimsabat@yahoo.com>
2018-11-22 23:02:28 +0000

    Add helper isLeapYear

--- codes/daysBetweebDays.py
+++ codes/daysBetweebDays.py
@@ -1,19 +1,29 @@
-
-def dateIsBefore(year1, month1, day1, year2, month2, day2):
-    """Returns True if year1-month1-day1 is before
-       year2-month2-day2. Otherwise, returns False."""
-    if year1 < year2:
+def isLeapYear(year):
+    if year % 400 == 0:
         return True
-    if year1 == year2:
-        if month1 < month2:
-            return True
-        if month1 == month2:
-            return day1 < day2
-    return False
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+def daysInMonth(year, month):
+    if month == 1 or month == 3 or  month == 5 or  month == 7 \
+       or month == 8 or  month == 10 or  month ==  12:
+        return 31
+    else:
+        if month == 2:
+           if isLeapYear(year):
+               return 29
+           else:
+               return 28
+        else:
+           return 30
+        return 30
 
 
 def nextDay(year,month,day):
-    if day < 30:
+    if day < daysInMonth(year, month):
         return (year, month, day+1)
     else:
         if day ==30:
