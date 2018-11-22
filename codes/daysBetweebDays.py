f4b333e885148b07e7ccd0f5f166dde370d73eeb
ibrahimshbat <ibrahimsabat@yahoo.com>
2018-11-22 22:04:59 +0000

    Add helper function dateIsBefore

---
+++ codes/daysBetweebDays.py
@@ -0,0 +1,35 @@
+
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    """Returns True if year1-month1-day1 is before
+       year2-month2-day2. Otherwise, returns False."""
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False
+
+
+def nextDay(year,month,day):
+    if day < 30:
+        return (year, month, day+1)
+    else:
+        if day ==30:
+            if month==12:
+               return (year+1, 1, 1)
+            else:
+               return (year, month+1,1)
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar."""
+    # program defensively! Add an assertion if the input is not valid!
+
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
