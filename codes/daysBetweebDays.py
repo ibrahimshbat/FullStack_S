f03612c248d6a778dcaa8d729921b84c4fa1b942
ibrahimshbat <ibrahimsabat@yahoo.com>
2018-11-22 22:06:19 +0000

    Add test cases

--- codes/daysBetweebDays.py
+++ codes/daysBetweebDays.py
@@ -33,3 +33,23 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
         year1, month1, day1 = nextDay(year1, month1, day1)
         days += 1
     return days
+
+def test():
+    test_cases = [((2012,9,30,2012,10,30),30),
+                  ((2012,1,1,2013,1,1),360),
+                  ((2012,9,1,2012,9,4),3),
+                  ((2013,1,1,1999,12,31), "AssertionError")]
+
+    for (args, answer) in test_cases:
+        try:
+            result = daysBetweenDates(*args)
+            if result != answer:
+                print "Test with data:", args, "failed"
+            else:
+                print "Test case passed!"
+        except AssertionError:
+            if answer == "AssertionError":
+                print "Nice job! Test case {0} correctly raises AssertionError!\n".format(args)
+            else:
+                print "Check your work! Test case {0} should not raise AssertionError!\n".format(args)
+test()
