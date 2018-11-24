9ddf22e0451d6289809c6ab16f2e21b69ff685fd
ibrahimshbat <ibrahimsabat@yahoo.com>
2018-11-24 19:43:30 +0000

    Add program to check + in str

---
+++ codes/check.py
@@ -0,0 +1,11 @@
+def printEx(a,b):
+    return a + b
+
+def printInche(a):
+    return str(a)+"Inches"
+
+n = pos()
+print n
+print printEx(2,7)
+print printEx("Ibrahim","Shbat")
+print printInche(55)
