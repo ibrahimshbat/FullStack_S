17792b9813c7cdeeed0ee2fa2c2fb0e51794c836
ibrahimshbat <ibrahimsabat@yahoo.com>
2018-12-20 23:15:42 -0800

    Add rename function

--- PythonExamles/renameFileName.py
+++ PythonExamles/renameFileName.py
@@ -1,4 +1,6 @@
 import os
 def rename_file():
-    file_list = os.listdir(r"E:\1MAC\FullStack_S\PythonExamles\prank")
-    print (file_list)
+    file_list = os.listdir(r"E:/1MAC/FullStack_S/PythonExamles/prank")
+    print file_list
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
