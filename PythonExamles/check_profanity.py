8fa4b2e2c49ee2de354d8e271f57a0cc420efd73
ibrahimshbat <ibrahimsabat@yahoo.com>
2018-12-28 22:33:17 -0800

    Add check_profanity function

--- PythonExamles/check_profanity.py
+++ PythonExamles/check_profanity.py
@@ -1,6 +1,18 @@
+import urllib
 def read_text():
     quotes = open("E:/1MAC/FullStack_S/PythonExamles/detect_profanity.txt")
     content_files = quotes.read()
     print(content_files)
     quotes.close()
+    check_profanity(content_files)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check)
+    output = connection.read()
+    connection.close()
+    if "true" in output:
+        print("Yes, there is course word in my text")
+    else:
+        print("No, there is not course word in my text")
+
 read_text()
