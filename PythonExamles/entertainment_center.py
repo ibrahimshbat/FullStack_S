54d25ae9ee4b73fbc81a84f2e4aa6272f446c0c8
ibrahimshbat <ibrahimsabat@yahoo.com>
2018-12-30 18:21:20 -0800

    Add entertainment

---
+++ PythonExamles/entertainment_center.py
@@ -0,0 +1,5 @@
+import media
+
+toy_Story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg", "https://www.youtube.com/watch?v=NBepTulmSMw")
+
+print (toy_Story.storyline)
---
+++ PythonExamles/media.py
@@ -0,0 +1,11 @@
+import webbroswer
+class Movie():
+    def __init__(self, movie_title, movie_storyline, movie_poster_image_url, movie_trailer_youtube_url):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster_image_url
+        self.trailer_youtube_url = movie_trailer_youtube_url
+
+def show_trailer(self):
+    webbroswer.open(self.trailer_youtube_url)
+    
